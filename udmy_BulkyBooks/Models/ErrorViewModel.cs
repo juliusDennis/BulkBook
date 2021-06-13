345c4c4937fab649c34a95138e3c4478876d8497
Julius <mjulius44@outlook.com>
2021-06-12 20:12:52 -0700

    Add project files.

---
+++ udmy_BulkyBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace udmy_BulkyBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
